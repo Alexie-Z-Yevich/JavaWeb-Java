47e8917ec9fc2933e2bc515c0489c7309b9ad8ed
FenrisX <1213791406@qq.com>
2022-05-18 21:05:10 +0800

    开始着手准备Spring Security的代码编写了

---
+++ src/main/java/com/springboot/admin/common/lang/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.springboot.admin.common.lang.exception;
+
+import com.springboot.admin.common.lang.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public Result handler(IllegalArgumentException e) {
+        log.error("Assert异常：--------------{}", e.getMessage());
+        return Result.fail(e.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = RuntimeException.class)
+    public Result handler(RuntimeException e) {
+        log.error("运行时异常：--------------{}", e.getMessage());
+        return Result.fail(e.getMessage());
+    }
+
+}
--- src/main/java/com/springboot/admin/controller/TestController.java
+++ src/main/java/com/springboot/admin/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.springboot.admin.controller;
 
+import com.springboot.admin.common.lang.Result;
 import com.springboot.admin.service.SysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
     @Autowired
-    SysUserService userService;
+    SysUserService sysUserService;
     @GetMapping("/test")
-    public Object test() {
-        return userService.list();
+    public Result test() {
+        return Result.succ(sysUserService.list());
     }
 }
