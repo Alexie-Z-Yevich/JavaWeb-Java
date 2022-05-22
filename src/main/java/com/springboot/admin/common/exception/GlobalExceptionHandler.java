b5465ddc137bda143443f32d1a7d30bdf28c889d
FenrisX <1213791406@qq.com>
2022-05-22 10:35:29 +0800

    菜单的增删改查接口开发

--- src/main/java/com/springboot/admin/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/springboot/admin/common/exception/GlobalExceptionHandler.java
@@ -3,6 +3,9 @@ package com.springboot.admin.common.exception;
 import com.springboot.admin.common.lang.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,7 +14,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    // 实体校验异常捕获
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public Result handler(MethodArgumentNotValidException e) {
 
+        BindingResult result = e.getBindingResult();
+        ObjectError objectError = result.getAllErrors().stream().findFirst().get();
+
+        log.error("实体校验异常：----------------{}", objectError.getDefaultMessage());
+        return Result.fail(objectError.getDefaultMessage());
+    }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = IllegalArgumentException.class)
--- src/main/java/com/springboot/admin/controller/SysMenuController.java
+++ src/main/java/com/springboot/admin/controller/SysMenuController.java
@@ -2,17 +2,19 @@ package com.springboot.admin.controller;
 
 
 import cn.hutool.core.map.MapUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.springboot.admin.common.dto.SysMenuDto;
 import com.springboot.admin.common.lang.Result;
+import com.springboot.admin.entity.SysMenu;
+import com.springboot.admin.entity.SysRoleMenu;
 import com.springboot.admin.entity.SysUser;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -47,6 +49,55 @@ public class SysMenuController extends BaseController {
                 .put("authorities", authorityInfoArray)
                 .put("nav", navs)
                 .map());
+    }
+
+    @GetMapping("/info/{id}")
+    @PreAuthorize("hasAuthority('sys:menu:list')")
+    public Result info(@PathVariable(name = "id") Long id) {
+        return Result.succ(sysMenuService.getById(id));
+    }
+
+    @GetMapping("/list")
+    @PreAuthorize("hasAuthority('sys:menu:list')")
+    public Result list() {
+        List <SysMenu> menus = sysMenuService.tree();
+        return Result.succ(menus);
+    }
+
+    @PostMapping("/save")
+    @PreAuthorize("hasAuthority('sys:menu:save')")
+    public Result save(@Validated @RequestBody SysMenu sysMenu) {
+        sysMenu.setCreated(LocalDateTime.now());
+        sysMenuService.save(sysMenu);
+        return Result.succ(sysMenu);
+    }
+
+    @PostMapping("/update")
+    @PreAuthorize("hasAuthority('sys:menu:update')")
+    public Result update(@Validated @RequestBody SysMenu sysMenu) {
+
+        sysMenu.setUpdated(LocalDateTime.now());
+        sysMenuService.updateById(sysMenu);
+
+        // 清除所有与该菜单相关的权限缓存
+        sysUserService.clearUserAuthorityInfoByMenuId(sysMenu.getId());
+        return Result.succ(sysMenu);
+    }
+
+    @PostMapping("/delete/{id}")
+    @PreAuthorize("hasAuthority('sys:menu:delete')")
+    public Result delete(@PathVariable(name = "id") Long id) {
+        int count = sysMenuService.count(new QueryWrapper<SysMenu>().eq("parent_id", id));
+        if(count > 0) {
+            return Result.fail("请先删除子菜单");
+        }
+        sysUserService.clearUserAuthorityInfoByMenuId(id);
+        sysMenuService.removeById(id);
+
+        // 同步删除中间关联表中的数据
+        sysRoleMenuService.remove(new QueryWrapper<SysRoleMenu>().eq("menu_id", id));
+
+        return Result.succ("");
     }
 
 }
